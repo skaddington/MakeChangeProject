e7f84fc3bcf22b2d0fce6018b6d257f610de933e
skaddington <skaddington92@gmail.com>
2023-03-18 20:39:33 -0600

    MakeChangeProject Complete

--- README.md
+++ README.md
@@ -2,11 +2,56 @@
 
 ## Overview
 
+- General Starter Setup
+	Import Scanner, declare a variable for it, close at the end of main method.
+	
+- User Story #1 & 2
+	Standard sysout println scanner initialization using the user input.
+
+- User Story #3
+	If, else if, and else statements using boolean expressions determine if the
+	amounts entered result in insufficient funds, exact amounts, or that change
+	is needed. For the first and second instances, sysout printlns say as much and
+	end the program.
+
+- User Story #4
+	The else statement is dedicated to calculating change using methods for each
+	denomination which then return the quantities of each.
+	Each denomination method begins by determining if it's needed at all using
+	boolean expressions ('change >= denomination value).
+	The methods utilize modulus, casting, and normal math functions.
+	When I reached the coin types, I decided that I needed to create a 'getCoins'
+	method and use the resulting 'coin' value for the coin denominations, factoring
+	out the dollar bills.
+	
+	Once all methods were complete, testing began using a plethora of values.
+	A road block was hit when I was unable to figure out why I was off by 1 penny
+	multiple times (Time for a break).
+
+	After a good nights sleep and time away from the screen, I figured it out!
+	We were given a hint that we might need to add 0.005 somewhere. After some trial
+	and error, I got everything in the right order.
+	Line 12: Declare & initialize 'change'
+	Line 13: Declare & initialize 'rounded' and use Math.round() to 2 decimal
+	places. This cleans up the number for when it prints out (Line 23).
+	Line 14: Then and only then add that 0.005 to 'change', make sure to use this in
+	the 'getCoins' method so that the pennies value is correct.
+
+- Moment of Truth
+	I tested all examples given in the instructions (multiple times): all good
+	I had my husband throw out random numbers: all good
+	Project #1 Complete!
 
 ## Technologies Used
 - Java
 - Eclipse
 - Git
+- Google - Stack Overflow
+- Saturday TA (Thank you!)
 
 ## Lessons Learned
-- 
+- Methods Inception
+- Modulus
+- Casting
+- Math functions (java.lang.Math)
+- if, else if, else
--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,9 +1,166 @@
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
 
+		System.out.println("Enter the receipt total: ");
+		double total = kb.nextDouble();
+		System.out.println("How much money was tendered? ");
+		double tendered = kb.nextDouble();
+		double change = tendered - total;
+		double rounded = Math.round(change * 100.0) / 100.0;
+		change = change + 0.005;
+		int coins = getCoins(change);
+
+		if (tendered < total) {
+			System.out.println("Insufficient Funds : Transaction Not Complete");
+		} else if (tendered == total) {
+			System.out.println("Exact Amount Received : No Change Needed");
+		} else {
+
+			System.out.println("Change to be given = " + rounded);
+			System.out.println("-------------------------------");
+
+			getTwenty(change);
+			getTen(change);
+			getFive(change);
+			getOne(change);
+			getQuarter(coins);
+			getDime(coins);
+			getNickel(coins);
+			getPenny(coins);
+
+		}
+
+		kb.close();
 	}
 
+	public static void getTwenty(double change) {
+		if (change >= 20.00) {
+			int twenty = (int) change / 20;
+			if (twenty > 1) {
+				System.out.println(twenty + " twenty dollar bills, ");
+			} else if (twenty == 1) {
+				System.out.println(twenty + " twenty dollar bill, ");
+			}
+		} else {
+			// Send nothing
+		}
+	}
+
+	public static void getTen(double change) {
+		if (change >= 10.00) {
+			int ten = (int) change / 10;
+			if (ten % 2 != 0) {
+				ten = 1;
+				System.out.println(ten + " ten dollar bill, ");
+			} else {
+				// Send nothing
+			}
+		} else {
+		}
+	}
+
+	public static void getFive(double change) {
+		if (change >= 5.00) {
+			int five = (int) change / 5;
+			if (five % 2 != 0) {
+				five = 1;
+				System.out.println(five + " five dollar bill, ");
+			} else {
+				// Send nothing
+			}
+		} else {
+		}
+	}
+
+	public static void getOne(double change) {
+		if (change >= 1.00) {
+			int remainder = (int) change / 5;
+			int one = (int) change - (remainder * 5);
+			if (one > 1) {
+				System.out.println(one + " one dollar bills, ");
+			} else if (one == 1) {
+				System.out.println(one + " one dollar bill, ");
+			} else {
+				// Send nothing
+			}
+		} else {
+		}
+	}
+
+	public static int getCoins(double change) {
+		int bills = (int) change;
+		int coins = (int) ((change - bills) * 100);
+		return coins;
+	}
+
+	public static void getQuarter(int coins) {
+		if (coins >= 25) {
+			int quarter = (int) coins / 25;
+			if (quarter > 1) {
+				System.out.println(quarter + " quarters, ");
+			} else if (quarter == 1) {
+				System.out.println(quarter + " quarter, ");
+			} else {
+				// Send nothing
+			}
+		} else {
+		}
+	}
+
+	public static void getDime(int coins) {
+		if (coins >= 10) {
+			int quarter = (int) coins / 25;
+			int remainder = coins - (25 * quarter);
+			int dime = (int) (remainder / 10);
+			if (dime > 1) {
+				System.out.println(dime + " dimes, ");
+			} else if (dime == 1) {
+				System.out.println(dime + " dime, ");
+			} else {
+				// Send nothing
+			}
+		} else {
+		}
+	}
+
+	public static void getNickel(int coins) {
+		if (coins >= 5) {
+			int quarter = (int) coins / 25;
+			int remainder = coins - (25 * quarter);
+			int dime = (int) (remainder / 10);
+			remainder = remainder - (10 * dime);
+			int nickel = (int) (remainder / 5);
+			if (nickel > 1) {
+				System.out.println(nickel + " nickels, ");
+			} else if (nickel == 1) {
+				System.out.println(nickel + " nickel, ");
+			} else {
+				// Send nothing
+			}
+		} else {
+		}
+	}
+
+	public static void getPenny(int coins) {
+		if (coins > 0) {
+			int nickel = (int) (coins / 5);
+			int penny = coins - (5 * nickel);
+			if (penny > 1) {
+				System.out.println(penny + " pennies. ");
+			} else if (penny == 1) {
+				System.out.println(penny + " penny. ");
+			} else {
+				// Send nothing
+			}
+		} else {
+		}
+	}
 }
+/*
+ * HINTS! modulus in action Math.round() add 0.005
+ * 
+ */
